e13a52dc8501899308e5d5b66c19987f6fbceeb0
uestcnlp <uestcnlp@gmail.com>
2019-01-09 16:01:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #stamp
-python3 cmain.py -m stamp_cikm -d cikm16
+python3 cmain.py -m stamp_cikm -d cikm16 -n
 
-python3 cmain.py -m stamp_rsc -d rsc15_64
+python3 cmain.py -m stamp_rsc -d rsc15_64 -n 
 
-python3 cmain.py -m stamp_rsc -d rsc15_4
+python3 cmain.py -m stamp_rsc -d rsc15_4 -n
 
 
